591743f27aeaabb0fa38fdd44388cd5f0d83c3d8
vigneshwar <vigneshwarasin1st@gmail.com>
2025-11-23 11:31:29 +0530

    Change QR code save file path to user input
    Updated file path for saving QR code image.

--- qrgene.py
+++ qrgene.py
@@ -1,8 +1,9 @@
 import qrcode
 url=input("enter the url: ").strip()
-file="C:\\Users\\sarun\\Desktop\\vit coll\\gene\\12.11.25\\qrcode.png"
+file="*************.png" """give the location of the pic to be saved"""
 qr =qrcode.QRCode()
 qr.add_data(url)
 img=qr.make_image()
 img.save(file)
+
 print("qr code is generated")
